atotto/clipboard

commit 55da149703a7cdce2926e4017316567511a1ab49

fixed runtime error and cleaned up xclip arguments

clipboard_linux.go
@@ -10,45 +10,42 @@ import (
 	"os/exec"
 )
 
+const (
+	xsel  = "xsel"
+	xclip = "xclip"
+)
+
 var (
-	pasteCmdArgs []string
-	copyCmdArgs  []string
+	pasteCmd, copyCmd *exec.Cmd
 
-	xselPaste = []string{"xsel", "--output", "--clipboard"}
-	xselCopy  = []string{"xsel", "--input", "--clipboard"}
+	xselPaste = exec.Command(xsel, "--output", "--clipboard")
+	xselCopy  = exec.Command(xsel, "--input", "--clipboard")
 
-	xclipPaste = []string{"xclip", "-out", "-secondary", "clipboard"}
-	xclipCopy  = []string{"xclip", "-in", "-secondary", "clipboard"}
+	xclipPaste = exec.Command(xclip, "-out", "-sel", "clipboard")
+	xclipCopy  = exec.Command(xclip, "-in", "-sel", "clipboard")
 )
 
 func init() {
-	_, err := exec.LookPath("xsel")
+	pasteCmd = xselPaste
+	copyCmd = xselCopy
+
+	_, err := exec.LookPath(xsel)
 	if err == nil {
-		pasteCmdArgs = xselPaste
-		copyCmdArgs = xselCopy
 		return
 	}
 
-	_, err = exec.LookPath("xclip")
+	pasteCmd = xclipPaste
+	copyCmd = xclipCopy
+
+	_, err = exec.LookPath(xclip)
 	if err == nil {
-		pasteCmdArgs = xclipPaste
-		copyCmdArgs = xclipCopy
 		return
 	}
 
 	println("No clipboard utilities available. Please install xset or xclip.")
 }
 
-func getPasteCommand() *exec.Cmd {
-	return exec.Command(pasteCmdArgs[0], pasteCmdArgs[1:]...)
-}
-
-func getCopyCommand() *exec.Cmd {
-	return exec.Command(copyCmdArgs[0], copyCmdArgs[1:]...)
-}
-
 func readAll() (string, error) {
-	pasteCmd := getPasteCommand()
 	out, err := pasteCmd.Output()
 	if err != nil {
 		return "", err
@@ -57,7 +54,6 @@ func readAll() (string, error) {
 }
 
 func writeAll(text string) error {
-	copyCmd := getCopyCommand()
 	in, err := copyCmd.StdinPipe()
 	if err != nil {
 		return err

commit 581c967adfd363d8b9f8287fc3e052ee402ea66a

Re-enable multiple calls for Linux
Was broken since 55da1497; added a test to prevent it from breaking
again.

clipboard_linux.go
@@ -16,26 +16,27 @@ const (
 )
 
 var (
-	pasteCmd, copyCmd *exec.Cmd
+	pasteCmdArgs []string
+	copyCmdArgs  []string
 
-	xselPaste = exec.Command(xsel, "--output", "--clipboard")
-	xselCopy  = exec.Command(xsel, "--input", "--clipboard")
+	xselPasteArgs = []string{xsel, "--output", "--clipboard"}
+	xselCopyArgs  = []string{xsel, "--input", "--clipboard"}
 
-	xclipPaste = exec.Command(xclip, "-out", "-selection", "clipboard")
-	xclipCopy  = exec.Command(xclip, "-in", "-selection", "clipboard")
+	xclipPasteArgs = []string{xclip, "-out", "-selection", "clipboard"}
+	xclipCopyArgs  = []string{xclip, "-in", "-selection", "clipboard"}
 )
 
 func init() {
-	pasteCmd = xselPaste
-	copyCmd = xselCopy
+	pasteCmdArgs = xselPasteArgs
+	copyCmdArgs = xselCopyArgs
 
 	_, err := exec.LookPath(xsel)
 	if err == nil {
 		return
 	}
 
-	pasteCmd = xclipPaste
-	copyCmd = xclipCopy
+	pasteCmdArgs = xclipPasteArgs
+	copyCmdArgs = xclipCopyArgs
 
 	_, err = exec.LookPath(xclip)
 	if err == nil {
@@ -45,7 +46,16 @@ func init() {
 	println("No clipboard utilities available. Please install xset or xclip.")
 }
 
+func getPasteCommand() *exec.Cmd {
+	return exec.Command(pasteCmdArgs[0], pasteCmdArgs[1:]...)
+}
+
+func getCopyCommand() *exec.Cmd {
+	return exec.Command(copyCmdArgs[0], copyCmdArgs[1:]...)
+}
+
 func readAll() (string, error) {
+	pasteCmd := getPasteCommand()
 	out, err := pasteCmd.Output()
 	if err != nil {
 		return "", err
@@ -54,6 +64,7 @@ func readAll() (string, error) {
 }
 
 func writeAll(text string) error {
+	copyCmd := getCopyCommand()
 	in, err := copyCmd.StdinPipe()
 	if err != nil {
 		return err

clipboard_test.go
@@ -27,6 +27,37 @@ func TestCopyAndPaste(t *testing.T) {
 	}
 }
 
+func TestMultiCopyAndPaste(t *testing.T) {
+	expected1 := "French: éèêëàùœç"
+	expected2 := "Weird UTF-8: 💩☃"
+
+	err := WriteAll(expected1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual1, err := ReadAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if actual1 != expected1 {
+		t.Errorf("want %s, got %s", expected1, actual1)
+	}
+
+	err = WriteAll(expected2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual2, err := ReadAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if actual2 != expected2 {
+		t.Errorf("want %s, got %s", expected2, actual2)
+	}
+}
+
 func BenchmarkReadAll(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ReadAll()